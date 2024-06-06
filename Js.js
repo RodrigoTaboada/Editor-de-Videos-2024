02d504eb4e5f3a83e5fa3ff565a397bfca6a1c11
Rodrigo <rodri.taboada@gmail.com>
2024-06-06 19:48:59 -0300

    1ero, BG BODY BLACK

---
+++ Js.js
@@ -0,0 +1,15 @@
+const cardHTML = document.getElementById(`Card`)
+
+const cards =`
+    <div class="mb-5 mx-5">
+        <label for="exampleFormControlTextarea1" class="form-label text-light ">Email</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Tú E-mail">
+        </div>
+        <div class="mb-3 mx-5">
+        <label for="exampleFormControlTextarea1" class="form-label text-light">Comentario</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>`;  
+   
+
+
+card.innerHTML = cards;
